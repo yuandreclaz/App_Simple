c3e822a5eb6bec3e2166f3c3c2d8da00108cd964
yuli cuadrado <andrelazaro1967@gmail.com>
2023-04-19 21:23:26 -0500

    primer commit

---
+++ hello.py
@@ -0,0 +1,79 @@
+
+
+from flask import Flask, render_template, url_for, request
+
+
+app = Flask(__name__)
+app.config.from_mapping(
+    SECRET_KEY = 'dev'
+)
+
+
+@app.add_template_filter
+def today(date):
+    return date.strftime('%d-%m-%Y')
+    
+
+@app.add_template_global  
+def repeat(s, n):        
+    return s * n
+
+
+
+from datetime import datetime
+
+# crear rutas
+@app.route('/')
+def index():
+    print(url_for('index'))
+    print(url_for('hello'))
+    name = 'Erika'
+    friends = ['Viviana', 'Andrés', 'Thomas', 'Gilberto']
+    date = datetime.now()
+    return render_template('index.html', 
+        name = name, 
+        friends = friends, 
+        date = date,    
+    )
+
+
+@app.route('/hello')
+@app.route('/hello/<name>')
+@app.route('/hello/<name>/<int:age>')
+@app.route('/hello/<name>/<int:age>/<email>')
+def hello(name = None, age = None, email = None):
+    
+    my_data = {
+        'name': name,
+        'age': age,
+        'email': email
+    }
+    return render_template('hello.html', data = my_data)
+
+
+
+# Crear formulario wtform
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+class RegisterForm(FlaskForm):
+    username = StringField('Nombre de usuario:', validators= [DataRequired(), Length(min=4, max=25)])
+    password = PasswordField('Password: ', validators= [DataRequired(), Length(min=6, max=40)])
+    submit = SubmitField('Registrar: ')
+
+
+
+# crear vista y ruta para Registrar usuario
+@app.route('/auth/register', methods = ['GET', 'POST'])
+
+def register():
+    #print(request.form) # muestra datos de ingreso en la terminal
+    form = RegisterForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        return f"Nombre de usuario: {username}, Contraseña: {password}"
+
+  
+    return render_template('auth/register.html', form = form)
